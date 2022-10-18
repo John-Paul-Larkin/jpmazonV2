593e2250df4acb4f1a028efc77e4ad6fafe0c2b0
john Paul Larkin <johnplarkin@gmail.com>
2022-10-18 13:15:30 +0100

    Netlify reload issue

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Body from "./Components/Body/Body";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProductPages from "./Components/ProductPages/ProductPages";
 import Footer from "./Components/Footer/Footer";
+import Test from "./Components/Body/Test";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
+          <Route path="/product/test" element={<Test />} />
           <Route path="/" element={<Body />} />
           <Route path="/product/:id" element={<ProductPages />} />
         </Routes>
