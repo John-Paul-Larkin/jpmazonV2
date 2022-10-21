d2178b7da94f39d426e9391dcca3d933fae03a68
john Paul Larkin <johnplarkin@gmail.com>
2022-10-21 19:30:59 +0100

    SHopping basket

--- src/Components/ProductPages/ShoppingBasket.jsx
+++ src/Components/ProductPages/ShoppingBasket.jsx
@@ -1,9 +1,32 @@
 import { ShoppingBasketContext } from "../../Hooks/useContext";
 import { useContext } from "react";
+
 import ShoppingBasketItems from "./ShoppingBasketItems";
 
 export default function ShoppingBasket() {
   const { basket } = useContext(ShoppingBasketContext);
+
+  const totalQuantityOfItems = () => {
+    if (basket.length == 0) {
+      return 0;
+    } else {
+      let total = 0;
+      basket.forEach((item) => (total += parseInt(item.quantity)));
+      return total;
+    }
+  };
+
+  const totalValue = () => {
+    if (basket.length === 0) {
+      return "0.00";
+    } else {
+      let total = 0;
+      basket.forEach((item) => {
+        total += item.quantity * item.price;
+      });
+      return total + ".00";
+    }
+  };
 
   return (
     <div className="shopping-basket">
@@ -12,6 +35,13 @@ export default function ShoppingBasket() {
         basket.map((item) => {
           return <ShoppingBasketItems item={item} key={item.id} />;
         })}
+      {!basket[0] && <div className="empty">Nothing here. Go spend some money!</div>}
+
+      <div className="sub-total-container">
+        <div className="subtotal">
+          Subtotal ({totalQuantityOfItems()} items) <span className="eur">EUR {totalValue()}</span>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Components/ProductPages/ShoppingBasketItems.jsx
+++ src/Components/ProductPages/ShoppingBasketItems.jsx
@@ -1,14 +1,68 @@
+import { useState } from "react";
 import "./ShoppingBasket.css";
 
+import { ShoppingBasketContext } from "../../Hooks/useContext";
+import { useContext } from "react";
+
 export default function ShoppingBasketItems({ item }) {
+  const { basket, setBasket } = useContext(ShoppingBasketContext);
+
+  const [quantity, setQuantity] = useState(item.quantity);
+
   return (
     <>
       <div className="item">
         <div className="img-container">
           <img src={item.thumbnail} alt="item basket pic" />
-          {item.quantity}
         </div>
-        <div className="order-details">f</div>
+        <div className="order-details">
+          <div className="title-container">
+            <div className="title">{item.title}</div>
+            <span className="price">EUR {item.price}.00</span>
+          </div>
+
+          <div className="stock">In stock</div>
+          <div className="eligible">Eligible for FREE shipping</div>
+          <div className="quantity-container">
+            <select
+              className="quantity"
+              name="Qty"
+              value={quantity}
+              onChange={(e) => {
+                item.quantity = e.target.value;
+
+                basket.map((eachItem) => {
+                  if (eachItem.id == item.id) {
+                    eachItem.quantity = e.target.value;
+                  }
+                  return eachItem;
+                });
+                setBasket([...basket]);
+                setQuantity(e.target.value);
+              }}
+            >
+              <option value="1">Qty: 1</option>
+              <option value="2">Qty: 2</option>
+              <option value="3">Qty: 3</option>
+              <option value="4">Qty: 4</option>
+              <option value="5">Qty: 5</option>
+              <option value="6">Qty: 6</option>
+              <option value="7">Qty: 7</option>
+              <option value="8">Qty: 8</option>
+              <option value="9">Qty: 9</option>
+              <option value="10">Qty: 10</option>
+            </select>
+            <div
+              onClick={() => {
+                const newBasket = basket.filter((basketItem) => basketItem.id != item.id);
+                setBasket([...newBasket]);
+              }}
+            >
+              Delete
+            </div>
+            <div>Save for later</div>
+          </div>
+        </div>
       </div>
     </>
   );
