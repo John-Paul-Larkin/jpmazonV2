d2d1974d9c9c93758adb3e113bb661dc81b5891b
john Paul Larkin <johnplarkin@gmail.com>
2022-11-13 12:07:47 +0000

    swiper cursor product page

--- src/Components/ProductPages/ProductPages.jsx
+++ src/Components/ProductPages/ProductPages.jsx
@@ -54,10 +54,15 @@ export default function ProductPages() {
             <br />
             <div className="images-container">
               {" "}
-              <Swiper className="main-image" modules={[FreeMode, Thumbs, Pagination]} pagination={true} thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}>
+              <Swiper
+                className="main-image"
+                modules={[FreeMode, Thumbs, Pagination]}
+                pagination={true}
+                thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
+              >
                 {product.images.map((image, index) => {
                   return (
-                    <SwiperSlide className="sw-slide" key={index}>
+                    <SwiperSlide key={index}>
                       <img src={image} alt="product" />
                     </SwiperSlide>
                   );
@@ -78,16 +83,19 @@ export default function ProductPages() {
             <div className="description-container">
               <p className="description">
                 <span>
-                  This is truly an amazing product. Although there are many competitors in the generic product category, this generic product, in particular, is the best that money can buy. It is perfectly
-                  sized, compact enough to store away, yet large enough that it wont get lost down the back of the couch. It is also made from a NASA researched, space age material, making it light enough to
-                  pop in your bun-bag, yet heavy enough that you could use it to bonk a bear on the head.
+                  This is truly an amazing product. Although there are many competitors in the generic product category, this generic product, in
+                  particular, is the best that money can buy. It is perfectly sized, compact enough to store away, yet large enough that it wont get
+                  lost down the back of the couch. It is also made from a NASA researched, space age material, making it light enough to pop in your
+                  bun-bag, yet heavy enough that you could use it to bonk a bear on the head.
                   <br />
                 </span>
                 {isShowMore && (
                   <span>
-                    Bear attacks are increasingly common, so for your protection, and the protection of your loved ones, click that buy button right now. It is colourful without being flashy. It is rounded, but
-                    also boxy. It is a little bit Ooh and a little bit Aah, just like the hit 90's tune. What a banger that was. Truly your life is not complete unless you buy this thing right now. You will be
-                    the envy of your friends. They will wish they also owned this generic product. People you are attracted to will be suitably impressed. What more can one write about such a unique item?
+                    Bear attacks are increasingly common, so for your protection, and the protection of your loved ones, click that buy button right
+                    now. It is colourful without being flashy. It is rounded, but also boxy. It is a little bit Ooh and a little bit Aah, just like
+                    the hit 90's tune. What a banger that was. Truly your life is not complete unless you buy this thing right now. You will be the
+                    envy of your friends. They will wish they also owned this generic product. People you are attracted to will be suitably impressed.
+                    What more can one write about such a unique item?
                   </span>
                 )}
               </p>
